4ae4bfb9ac0e212dcb44e3d70cc9182e5614dc26
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-16 10:10:32 -0300

    Validacion de form listo

--- src/app/pages/alto-actor/alto-actor.component.ts
+++ src/app/pages/alto-actor/alto-actor.component.ts
@@ -2,11 +2,19 @@ import { Component, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ApiService } from '../../services/api.service';
 import { Pais } from '../../models/pais';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-alto-actor',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './alto-actor.component.html',
   styleUrl: './alto-actor.component.css',
 })
@@ -14,6 +22,17 @@ export class AltoActorComponent {
   sub?: Subscription;
   private apiRest: ApiService = inject(ApiService);
   listaPaises: Pais[] = [];
+  public fb: FormBuilder = inject(FormBuilder);
+  public fg: FormGroup;
+
+  constructor() {
+    this.fg = this.fb.group({
+      nombre: ['', [Validators.required, this.noCaracteresEspeciales]],
+      apellido: ['', [Validators.required, this.noCaracteresEspeciales]],
+      documento: ['', [Validators.required, this.validDocument]],
+      edad: ['', [Validators.required, this.validDocument]],
+    });
+  }
 
   ngOnInit(): void {
     this.sub = this.apiRest.getPaises().subscribe((paises: any) => {
@@ -26,14 +45,42 @@ export class AltoActorComponent {
 
         this.listaPaises.push(pais);
       });
+      this.listaPaises.sort((a, b) => a.nombre.localeCompare(b.nombre));
     });
   }
 
-  ngAfterViewInit(): void {
-    console.log(this.listaPaises);
-  }
+  ngAfterViewInit(): void {}
 
   ngOnDestroy(): void {
     this.sub?.unsubscribe();
+  }
+
+  acceder() {
+    if (this.fg.valid) {
+      //Cargar a la base de datos
+      console.log('Se cargo con exito');
+    } else {
+      console.log(this.fg.controls['documento'].errors);
+      console.log('sos un boludo');
+    }
+  }
+
+  // Validador personalizado
+  noCaracteresEspeciales(control: any) {
+    const regex = /^[a-zA-Z]+$/;
+    if (control.value && !regex.test(control.value)) {
+      return { caracteresEspeciales: true };
+    }
+    return null;
+  }
+
+  validDocument(control: any) {
+    if (
+      Number.parseInt(control.value) < 1000000 ||
+      Number.parseInt(control.value) > 99999999
+    ) {
+      return { cantidadDoc: true };
+    }
+    return null;
   }
 }
